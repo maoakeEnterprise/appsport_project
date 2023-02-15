646c49675d95a4ed10bf57294e5a18345893dee5
Maoake TERIIEROOITERAI <90271704+maoakeEnterprise@users.noreply.github.com>
2023-02-15 22:21:14 +0100

    Doing my BD

--- lib/firebase/exercicefirebase.dart
+++ lib/firebase/exercicefirebase.dart
@@ -1,7 +1,6 @@
 import 'package:appsport_project/bloc/exercicebloc/exercice_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ExerciceFirebase extends StatelessWidget {
