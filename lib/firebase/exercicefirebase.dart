a94ce20ea1c90401578ac1f7a4d32835f67a7004
Maoake TERIIEROOITERAI <90271704+maoakeEnterprise@users.noreply.github.com>
2023-02-18 14:42:13 +0100

    Struggle on the query

--- lib/firebase/exercicefirebase.dart
+++ lib/firebase/exercicefirebase.dart
@@ -1,12 +1,19 @@
 import 'package:appsport_project/bloc/exercicebloc/exercice_bloc.dart';
+import 'package:appsport_project/firebase/queryService.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ExerciceFirebase extends StatelessWidget {
   ExerciceFirebase({Key? key}) : super(key: key);
-  final Stream<QuerySnapshot> informationExercice =
-      FirebaseFirestore.instance.collection('Exercices').snapshots();
+  FirebaseFirestore db = FirebaseFirestore.instance;
+  final Stream<QuerySnapshot> informationExercice = FirebaseFirestore.instance.collection('Exercices').snapshots();
+  bool flag = false;
+  var listExo = [];
+  var stateActual;
+
+
+  
 
   @override
   Widget build(BuildContext context) {
@@ -30,9 +37,28 @@ class ExerciceFirebase extends StatelessWidget {
                     snapshot.data!.docs.map((DocumentSnapshot document) {
                       Map<String, dynamic> data =
                       document.data()! as Map<String, dynamic>;
-                      if(data['type'] == state.type){
+                      if(state.type != stateActual) {
+                        print("test");
+                        listExo = [];
+                        QueryService().getQueryMuscle("${state.type}").then(
+                          (QuerySnapshot docs){
+                            if(docs.docs.isNotEmpty){
+                              //listExo = [];
+                              for(int i=0;i < docs.size ; i++){
+                                if(data['idmuscle'] == docs.docs[i].get('id')){
+                                  flag = true;
+                                  listExo.add(data['idmuscle']);
+                                  print(data['nom']);
+                                }
+                              }
+                            }
+                            stateActual = state.type;
+                          }
+                      );
+                      }
+                      if(listExo != null){
                         return ListTile(
-                          title: Text(data['nom']),
+                          title: Text("${listExo.length}"),
                         );
                       }
                       else{
@@ -66,7 +92,6 @@ class ExerciceFirebase extends StatelessWidget {
                   );
                 });
           }
-
         },
       ),
     );
